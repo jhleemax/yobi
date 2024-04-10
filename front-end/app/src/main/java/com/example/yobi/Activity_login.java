fd66ec9c46405f243e0c800ca3af9d5c73bfad8b
ijeongho <jhleemax@gmail.com>
2024-04-10 12:08:20 +0900

    splash, login, register(incomplete) screen

---
+++ front-end/app/src/main/java/com/example/yobi/Activity_login.java
@@ -0,0 +1,36 @@
+package com.example.yobi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView temp = findViewById(R.id.textView_login_register);
+        temp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity_login.this, Activity_register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
