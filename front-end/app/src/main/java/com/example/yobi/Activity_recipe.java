de2fdf9c583dceda9b2403cf989135f850ab708d
jhleemax <jhleemax@gmail.com>
2024-05-21 22:39:09 +0900

    Apply Design(~activity_recipe)

--- front-end/app/src/main/java/com/example/yobi/Activity_recipe.java
+++ front-end/app/src/main/java/com/example/yobi/Activity_recipe.java
@@ -2,16 +2,10 @@ package com.example.yobi;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,13 +15,6 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-import com.squareup.picasso.Picasso;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 public class Activity_recipe extends AppCompatActivity {
@@ -61,7 +48,7 @@ public class Activity_recipe extends AppCompatActivity {
 
         context = this;
 
-        acButton_home = findViewById(R.id.appCompatButton_main_home);
+        acButton_home = findViewById(R.id.appCompatButton_recipe_home);
         acButton_home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
