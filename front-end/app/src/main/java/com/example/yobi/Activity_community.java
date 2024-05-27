0c3642786830288b4c0186e2581e1ec50d538b97
jhleemax <jhleemax@gmail.com>
2024-05-27 10:47:33 +0900

    (NEW) Activity_community (NEW) Activity_community_write

--- front-end/app/src/main/java/com/example/yobi/Activity_community.java
+++ front-end/app/src/main/java/com/example/yobi/Activity_community.java
@@ -1,9 +1,6 @@
 package com.example.yobi;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Activity_main extends AppCompatActivity {
-    Button acButton_recipe;
+public class Activity_community extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_community);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        acButton_recipe = findViewById(R.id.appCompatButton_community_recipe);
-        acButton_recipe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Activity_main.this, Activity_recipe.class);
-                startActivity(intent);
-            }
         });
     }
 }
