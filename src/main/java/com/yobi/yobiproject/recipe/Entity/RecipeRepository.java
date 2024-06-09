2fbd5b658917617cb262df02f2f0f19d2605ddb7
Seodol <seohb0311@gmail.com>
2024-06-09 21:08:24 +0900

    레시피 작성

--- src/main/java/com/yobi/yobiproject/recipe/Entity/RecipeRepository.java
+++ src/main/java/com/yobi/yobiproject/recipe/Entity/RecipeRepository.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> { // Integer는 기본키 타입
+    long countByUserId(String userId); //사용자가 작성한 레시피 개수 가져오기
     Recipe findByRecipeNum(int recipeNum);
     List<Recipe> findAll();
     List<Recipe> findAllByFoodNameContaining(String foodName);
--- src/main/java/com/yobi/yobiproject/recipe/dto/WriteRecipeDTO.java
+++ src/main/java/com/yobi/yobiproject/recipe/dto/WriteRecipeDTO.java
@@ -15,62 +15,23 @@ public class WriteRecipeDTO {
     private String ingredient;
 
     private String recipeManual01;
-    private String recipeImage01;
-
     private String recipeManual02;
-    private String recipeImage02;
-
     private String recipeManual03;
-    private String recipeImage03;
-
     private String recipeManual04;
-    private String recipeImage04;
-
     private String recipeManual05;
-    private String recipeImage05;
-
     private String recipeManual06;
-    private String recipeImage06;
-
     private String recipeManual07;
-    private String recipeImage07;
-
     private String recipeManual08;
-    private String recipeImage08;
-
     private String recipeManual09;
-    private String recipeImage09;
-
     private String recipeManual10;
-    private String recipeImage10;
-
     private String recipeManual11;
-    private String recipeImage11;
-
     private String recipeManual12;
-    private String recipeImage12;
-
     private String recipeManual13;
-    private String recipeImage13;
-
     private String recipeManual14;
-    private String recipeImage14;
-
     private String recipeManual15;
-    private String recipeImage15;
-
     private String recipeManual16;
-    private String recipeImage16;
-
     private String recipeManual17;
-    private String recipeImage17;
-
     private String recipeManual18;
-    private String recipeImage18;
-
     private String recipeManual19;
-    private String recipeImage19;
-
     private String recipeManual20;
-    private String recipeImage20;
 }
